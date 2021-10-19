f4a143ceaf2ed222a23fc76c2ee14af07e698de8
tinas <ahmettinastepe0@gmail.com>
2021-10-19 19:08:13 +0300

    Implement action sheet component

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { StyleSheet, Image, Linking } from 'react-native'
+
+import ActionSheet from 'react-native-actions-sheet'
+import HTMLView from 'react-native-htmlview'
 
 import Box from './box'
+import Label from './label'
 import Button from './button'
 import Logo from './logo'
 import { Megaphone, Info } from './icons'
 
 import theme from '../utils/theme'
+
+const actionSheetRef = React.createRef()
 
 const NavBar = ({ ...props }) => {
   return (
@@ -16,13 +23,69 @@ const NavBar = ({ ...props }) => {
           <Button>
             <Megaphone width={32} height={32} color={theme.colors.icon} />
           </Button>
-          <Button pl={24}>
+          <Button
+            pl={24}
+            onPress={() => {
+              actionSheetRef.current?.setModalVisible()
+            }}
+          >
             <Info width={32} height={32} color={theme.colors.icon} />
           </Button>
         </Box>
       </Box>
+      <ActionSheet ref={actionSheetRef} gestureEnabled={true}>
+        <Box alignItems="center" px={16}>
+          <Logo mt={16} fontSize={30} />
+          <Box mt={16}>
+            <HTMLView value={htmlContent} stylesheet={styles} />
+          </Box>
+          <Label mt={16} fontSize={20} color="green">
+            Katkıda bulunun!
+          </Label>
+          <Button
+            width="100%"
+            mt={8}
+            py={8}
+            px={26}
+            borderRadius={8}
+            backgroundColor="green"
+            onPress={() => {
+              Linking.openURL('https://kodilan.com/').catch((err) => {
+                console.error('Failed opening page because: ', err)
+                alert('Failed to open page')
+              })
+            }}
+          >
+            <Image style={{ width: 32, height: 32 }} source={require('../assets/github.png')} />
+            <Label ml={16} fontSize={16} color="white" fontWeight="700">
+              kodilan-com
+            </Label>
+          </Button>
+          <Label mt={32} color="disable">
+            kodilan.com açık kaynaklı bir girişimdir.
+          </Label>
+        </Box>
+      </ActionSheet>
     </Box>
   )
 }
+
+const htmlContent = `<p><strong>Kodilan.com</strong> ilan yayınlama sitesidir. Başvurular ilgili firmaya direkt olarak yapılmakta olup <strong>Kodilan.com</strong>
+üzerinden hiçbir veri gönderilmemektedir. Sitemizde yayınlanan ilanlar tamamen ücretsizdir ve ilan sahipleri
+ile sitemizin hiçbir ilişkisi yoktur. Paylaşılan ilanlarda değişiklik yapma ve yayından kaldırma hakları
+Kodilan'a aittir. Görüş ve önerileriniz için <strong>info@kodilan.com</strong> adresine e-posta gönderebilirsiniz.</p>`
+
+const styles = StyleSheet.create({
+  p: {
+    color: theme.colors.disable,
+    lineHeight: 30,
+    textAlign: 'center'
+  },
+
+  strong: {
+    color: theme.colors.green,
+    fontWeight: '700'
+  }
+})
 
 export default NavBar
